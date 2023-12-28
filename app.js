42c91b580c551902170667fcedbe79a8bf0f8256
Harshal Choudhary <111169576+harshalchoudhary11@users.noreply.github.com>
2023-12-28 10:37:13 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,143 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const yearsSelect = document.getElementById("yearSelect");
+    const categorySelect = document.getElementById("categorySelect");
+    const winnersList = document.getElementById("winnersList");
+  
+    let years = [];
+    let categories = [];
+    let selectedYear = "";
+    let selectedCategory = "";
+    let prizes = [];
+  
+    async function fetchPrizeData() {
+      try {
+        const response = await fetch("https://api.nobelprize.org/v1/prize.json");
+        const data = await response.json();
+        const prizeData = data.prizes || [];
+        prizes = prizeData;
+  
+        years = Array.from({ length: 2019 - 1900 }, (_, index) => 1900 + index);
+        setSelectOptions(yearsSelect, years);
+  
+        categories = [...new Set(prizeData.map(prize => prize.category))];
+        setSelectOptions(categorySelect, categories);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+  
+    function displayWinners() {
+      const filteredPrizes = prizes.filter(prize => prize.year == selectedYear && prize.category == selectedCategory);
+      winnersList.innerHTML = filteredPrizes.map(createWinnerListItem).join('');
+    }
+  
+    function createWinnerListItem(prize) {
+      return `
+        <li key="${prize.year + prize.category}">
+          <strong id="YearName" class="text-white" >
+            Year: ${prize.year}, Category: ${prize.category.toUpperCase()}
+          </strong>
+          <ul>
+            ${prize.laureates.map(laureate => `
+              <li key="${laureate.id}"id="unique" class="text-blue">
+                ${laureate.firstname} ${laureate.surname}
+              </li>`).join('')}
+          </ul>
+        </li>`;
+    }
+  
+    function handleSelectChange(event, property) {
+      const selectedValue = event.target.value;
+      if (property === 'year') {
+        selectedYear = selectedValue;
+      } else if (property === 'category') {
+        selectedCategory = selectedValue;
+      }
+      displayWinners();
+    }
+  
+    function setSelectOptions(selectElement, options) {
+      options.forEach(option => {
+        const optionElement = document.createElement("option");
+        optionElement.value = option;
+        optionElement.text = option;
+        selectElement.add(optionElement);
+      });
+    }
+  
+    yearsSelect.addEventListener("change", event => handleSelectChange(event, 'year'));
+    categorySelect.addEventListener("change", event => handleSelectChange(event, 'category'));
+  
+    fetchPrizeData();
+  });
+  
+
+//     document.addEventListener("DOMContentLoaded", function () {
+//     const yearsSelect = document.getElementById("yearSelect");
+//     const categorySelect = document.getElementById("categorySelect");
+//     const winnersList = document.getElementById("winnersList");
+
+//     let years = [];
+//     let categories = [];
+//     let selectedYear = "";
+//     let selectedCategory = "";
+//     let prizes = [];
+
+//     function fetchPrizeData() {
+//         fetch("https://api.nobelprize.org/v1/prize.json")
+//             .then(response => response.json())
+//             .then(data => {
+//                 const prizeData = data.prizes || [];
+//                 prizes = prizeData;
+
+//                 years = Array.from({ length: 2019 - 1900 }, (_, index) => 1900 + index);
+//                 setSelectOptions(yearsSelect, years);
+
+//                 categories = [...new Set(prizeData.map(prize => prize.category))];
+//                 setSelectOptions(categorySelect, categories);
+//             })
+//             .catch(error => {
+//                 console.error("Error fetching data:", error);
+//             });
+//     }
+
+//     function displayWinners() {
+//         const filteredPrizes = prizes.filter(prize => prize.year == selectedYear && prize.category == selectedCategory);
+//         winnersList.innerHTML = filteredPrizes.map(prize => `
+//             <li key="${prize.year + prize.category}">
+//                 <strong class="text-white">
+//                     Year: ${prize.year}, Category: ${prize.category}
+//                 </strong>
+//                 <ul>
+//                     ${prize.laureates.map(laureate => `
+//                         <li key="${laureate.id}" class="text-blue">
+//                             ${laureate.firstname} ${laureate.surname}
+//                         </li>`).join('')}
+//                 </ul>
+//             </li>`).join('');
+//     }
+
+//     function handleYearChange(event) {
+//         selectedYear = event.target.value;
+//         displayWinners();
+//     }
+
+//     function handleCategoryChange(event) {
+//         selectedCategory = event.target.value;
+//         displayWinners();
+//     }
+
+//     function setSelectOptions(selectElement, options) {
+//         options.forEach(option => {
+//             const optionElement = document.createElement("option");
+//             optionElement.value = option;
+//             optionElement.text = option;
+//             selectElement.add(optionElement);
+//         });
+//     }
+
+//     yearsSelect.addEventListener("change", handleYearChange);
+//     categorySelect.addEventListener("change", handleCategoryChange);
+
+//     fetchPrizeData();
+// });
